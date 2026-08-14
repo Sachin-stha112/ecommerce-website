4b0a65e54c4ff6c76fdf7c40f0d2fd3b9ac5a39b
Sachin stha <chensthaii@gmail.com>
2026-08-14 18:28:55 +0545

    Added home page with product grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { getProducts } from '../data/products'
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
+  const products = getProducts();
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='page'>
+      <div className="home-hero">
+        <h1 className='home-title'>Welcome to ShopHub</h1>
+        <p className='home-subtitle'>Discover amazing products for great price</p>
+      </div>
+      <div className="container">
+        <h2 className='page-title'>Our Products</h2>
+        <div className="product-grid">
+          {products.map((product) => (
+            <ProductCard product={product} key={product.id} />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
