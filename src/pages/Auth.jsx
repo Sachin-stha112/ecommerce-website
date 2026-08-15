2f980ade9f212c09fc2dbbe12a4f03cfa7377b4d
Sachin stha <chensthaii@gmail.com>
2026-08-15 19:18:41 +0545

    Implemented useForm for login and signup forms

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { useState } from 'react'
 
 const Auth = () => {
   const [mode, setMode] = useState("signup")
+  const {register, handleSubmit, formState:{errors}} = useForm();
+
+  function onSubmit()
+  {
+    alert("Signed Up")
+  }
+
   return (
     <div className="page">
       <div className="container">
@@ -10,14 +18,28 @@ const Auth = () => {
           <h1 className='page-title'>
             {mode == "signup" ? "Sign Up" : "Login"}
           </h1>
-          <form className='auth-form'>
+          <form className='auth-form' onSubmit={handleSubmit(onSubmit)}>
             <div className="form-group">
               <label className='form-label' htmlFor='email'>Email</label>
-              <input className='form-input' type='email' id='email'/>
+              <input className='form-input' type='email' id='email'
+              {...register("email", {required : "Username is required"})}
+              />
             </div> 
             <div className="form-group">
               <label className='form-label' htmlFor='password'>Password</label>
-              <input className='form-input' type='password' id="password" />
+              <input className='form-input' type='password' id="password"
+              {...register("password", 
+                {required : "Password is required",
+                  minLength : {
+                    value: 6,
+                    message : "Password should be atleast 6 characters"
+                  },
+                  maxLength : {
+                    value: 20,
+                    message : "Password should be atmost 20 characters"
+                  }
+                })}
+              />
             </div>
             <button type='submit' className='btn btn-primary btn-large'>{mode == "signup" ? "Sign Up": "Login"}</button>
           </form>
