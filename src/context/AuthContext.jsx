6be458939e1d5419f02def8f82a4a59166c1f86a
Sachin stha <chensthaii@gmail.com>
2026-08-16 13:39:54 +0545

    Added signUp function to AuthContext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { createContext, useState, useContext } from 'react'
+
+const AuthContext = createContext(null)
+
+export default function AuthProvider({children})
+{
+  const [user, setUser] = useState(
+    localStorage.getItem("currentUserEmail")
+    ? {email: localStorage.getItem("currentUserEmail")}
+     : null
+  )
+
+  function signUp(email, password)
+  {
+    const users = JSON.parse(localStorage.getItems("users") || "[]");
+
+    if (users.find((u) => u.email === email) )
+    {
+      return {success: false, error: "Email alreadt exits"}
+    }
+    const newUser = {email, password}
+    users.push(newUser)
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUserEmail", email)
+
+    setUser({email})
+
+    return {success : true}
+  }
+
+}
