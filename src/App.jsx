464056c7c30e872cee00c099f8d2b2656f17e4d3
Sachin stha <chensthaii@gmail.com>
2026-08-17 09:10:54 +0545

    Added product details page with dynamic route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
+import ProductDetails from './pages/ProductDetails'
 
 import './App.css'
 
@@ -17,6 +18,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/auth' element={<Auth />} />
         <Route path='/checkout' element={<Checkout />} />
+        <Route path = '/products/:id' element ={< ProductDetails/>} />
       </Routes>
     </div>
   )
