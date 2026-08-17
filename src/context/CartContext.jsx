f43d5387cf8cff33905962a07c27eb87e1d07de2
Sachin stha <chensthaii@gmail.com>
2026-08-17 11:16:00 +0545

    Added CartContext with cart management functions

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { createContext, useState, useContext } from 'react'
+
+const CartContext = createContext(null)
+
+export default function CartProvider({children})
+{
+  const [cartItems, setCartItems] = useState([])
+
+  function addToCart(productId)
+  {
+    const existing = cartItems.find((item) => item.id === productId);
+    if(existing)
+    {
+      const currentQuantity= existing.quantity
+      const updatedCartItems = cartItems.map((item) => item.id === productId
+      ? {id: productId, quantity: currentQuantity + 1} : item )
+    }
+    else
+    {
+      setCartItems([...cartItems, {id : productId, quantity : 1}])
+    }
+  }
+
+  return (
+    <CartContext.Provider value={{cartItems, addToCart}} >
+      {children}
+    </CartContext.Provider>
+  )
+
+}
+// custom hook for useContext(CartContext)
+export function useCart()
+{
+  const context = useContext(CartContext);
+  return context;
+}
